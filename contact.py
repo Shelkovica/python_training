9d337e34385b22411dc546cf55c2ee0f315833a7
Victoria <senvica@mail.ru>
2019-08-24 18:25:30 +0400

    Задание №3

---
+++ contact.py
@@ -0,0 +1,29 @@
+class Contact:
+    def __init__(self, firstname, lastname, middlename, nickname, company, address, home, mobile, work, fax,
+                       email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2, phone2,
+                       notes, title1):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear= byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+        self.title1 = title1
+
