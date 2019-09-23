a103b15c5396dcf02b964a3dc0a9f0e8e6c5d50a
Victoria <senvica@mail.ru>
2019-09-23 21:47:03 +0400

    Задание №17

--- conftest.py
+++ conftest.py
@@ -1,24 +1,25 @@
 from fixture.application import Application
 import pytest
+import json
+import os.path
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    local_url = request.config.getoption("--localUrl")
-    username = request.config.getoption("--username")
-    password = request.config.getoption("--password")
-    local_group_url = request.config.getoption("--localGroupUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url, username=username, password=password, local_url=local_url, local_group_url = local_group_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url, username=username, password=password, local_url=local_url, local_group_url = local_group_url)
-    fixture.session.ensure_login(username=username, password=password)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'], username=target['username'], password=target['password'],
+                              local_url=target['localUrl'], local_group_url = target['localGroupUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -33,8 +34,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--localUrl", action="store", default="/addressbook/")
-    parser.addoption("--localGroupUrl", action="store", default="/group.php")
-    parser.addoption("--username", action="store", default="admin")
-    parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
+
