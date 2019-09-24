fc921baf6d24e6a0f65096ffe32d7e005c7607d9
Victoria <senvica@mail.ru>
2019-09-24 14:16:16 +0400

    Задание №18

--- data/contacts.py
+++ data/contacts.py
@@ -4,7 +4,7 @@ import random
 import string
 
 
-constant = [
+testdata = [
     Contact(firstname="firstname1", lastname="lastname1", address="address1",
             homephone="homephone1", mobilephone="mobilephone1", workphone="workphone1", email="email1", email2="email2", email3="email3",
             secondaryphone="secondaryphone1"),
@@ -14,22 +14,22 @@ constant = [
 ]
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters+string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#def random_string(prefix, maxlen):
+ #   symbols = string.ascii_letters+string.digits
+ #   return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def random_email(maxlen):
-    symbols = string.ascii_letters + string.digits
-    email_address = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    email_domain = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-    return email_address+"@"+email_domain+"."+"".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#def random_email(maxlen):
+ #   symbols = string.ascii_letters + string.digits
+  #  email_address = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+   # email_domain = "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+   # return email_address+"@"+email_domain+"."+"".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata =  [Contact(firstname="", lastname="", address="",
-            homephone="", mobilephone="", workphone="", email="", email2="", email3="", secondaryphone="")]+ [
-    Contact(firstname=random_string("first",10), lastname=random_string("las",10),
-            address=random_string("addr",10), homephone=random_string("",10), mobilephone=random_string("",10),
-            workphone=random_string("",10), secondaryphone=random_string("",10), email=random_email(5), email2=random_email(6),
-            email3=random_email(5))
-for i in range (10)
-]
+#testdata =  [Contact(firstname="", lastname="", address="",
+#            homephone="", mobilephone="", workphone="", email="", email2="", email3="", secondaryphone="")]+ [
+#    Contact(firstname=random_string("first",10), lastname=random_string("las",10),
+#            address=random_string("addr",10), homephone=random_string("",10), mobilephone=random_string("",10),
+#            workphone=random_string("",10), secondaryphone=random_string("",10), email=random_email(5), email2=random_email(6),
+#            email3=random_email(5))
+#for i in range (10)
+#]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,11 +3,12 @@ import pytest
 from model.contact import Contact
 import random
 import string
-from data.contacts import constant as testdata
+#from data.contacts import constant as testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+#@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
