1f2c79bbb6ad20fdbe6e6da830affe6d2b163475
Victoria <senvica@mail.ru>
2019-10-02 22:54:33 +0400

    Задание №21

--- fixture/orm.py
+++ fixture/orm.py
@@ -22,6 +22,14 @@ class ORMFixture:
         id = PrimaryKey(int, column='id')
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
+        address = Optional(str, column='address')
+        secondaryphone = Optional(str, column='phone2')
+        mobilephone = Optional(str, column='mobile')
+        homephone = Optional(str, column='home')
+        workphone = Optional(str, column='work')
+        email = Optional(str, column='email')
+        email2 = Optional(str, column='email2')
+        email3 = Optional(str, column='email3')
         deprecated = Optional(datetime, column='deprecated')
         groups = Set(lambda: ORMFixture.ORMGroup, table='address_in_groups', column='group_id', reverse='contacts', lazy=True)
 
@@ -37,7 +45,9 @@ class ORMFixture:
 
     def convert_contacts_to_model(self, contacts):
         def convert(contact):
-            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname, address=contact.address,
+                           secondaryphone=contact.secondaryphone, mobilephone=contact.mobilephone, homephone=contact.homephone,
+                           workphone=contact.workphone, email=contact.email, email2=contact.email2, email3=contact.email3)
         return list(map(convert, contacts))
 
     @db_session
--- test/test_phones.py
+++ test/test_phones.py
@@ -4,10 +4,10 @@ from fixture.orm import ORMFixture
 from model.contact import Contact
 
 
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+#def test_phones_on_home_page(app):
+ #   contact_from_home_page = app.contact.get_contact_list()[0]
+ #   contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+ #   assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 #def test_phones_on_contact_view_page(app):
@@ -19,15 +19,21 @@ def test_phones_on_home_page(app):
    # assert contact_from_view_page.homephone == contact_from_edit_page.homephone
 
 
-def test_all_data_contact_on_home_page(app):
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+def test_all_data_contact_on_home_page(app, orm):
+    contacts_from_bd = orm.get_contact_list()
+    if len(orm.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="test contact 0", lastname="000"))
+    contacts_on_home_page = sorted(app.contact.get_contact_list(),  key=Contact.id_or_max)
+    contacts_from_bd = sorted(orm.get_contact_list(), key=Contact.id_or_max)
+    index = len(orm.get_contact_list())
+    for i in [0, index-1]:
+        assert contacts_on_home_page[i].lastname == contacts_from_bd[i].lastname
+        assert contacts_on_home_page[i].firstname == contacts_from_bd[i].firstname
+        assert contacts_on_home_page[i].all_phones_from_home_page == merge_phones_like_on_bd(contacts_from_bd[i])
+        assert contacts_on_home_page[i].lastname == contacts_from_bd[i].lastname
+        assert contacts_on_home_page[i].firstname == contacts_from_bd[i].firstname
+        assert contacts_on_home_page[i].address == contacts_from_bd[i].address
+        assert contacts_on_home_page[i].all_emails_from_home_page == merge_emails_like_on_bd(contacts_from_bd[i])
 
 
 def clear(s):
@@ -45,15 +51,24 @@ def merge_emails_like_on_home_page(contact):
                             map(lambda x: x, filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
 
 
-def test_all_data_contact_on_home_page_orm(app, orm):
-    if len(orm.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="test contact 0", lastname="000"))
-    contact_old = sorted(app.contact.get_contact_list(),  key=Contact.id_or_max)
-    contact_orm = sorted(orm.get_contact_list(), key=Contact.id_or_max)
-    index = len(contact_old)
-    for i in [0, index-1]:
-        assert contact_old[i].lastname == contact_orm[i].lastname
-        assert contact_old[i].firstname == contact_orm[i].firstname
+def merge_phones_like_on_bd(contact):
+    return "\n".join(filter(lambda x: x!="",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None, [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+
+def merge_emails_like_on_bd(contact):
+    return "\n".join(filter(lambda x: x!="",
+                            map(lambda x: x, filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
+
+#def test_all_data_contact_on_home_page_orm(app, orm):
+ #   if len(orm.get_contact_list()) == 0:
+  #      app.contact.create(Contact(firstname="test contact 0", lastname="000"))
+  #  contact_old = sorted(app.contact.get_contact_list(),  key=Contact.id_or_max)
+  #  contact_orm = sorted(orm.get_contact_list(), key=Contact.id_or_max)
+  #  index = len(contact_old)
+  #  for i in [0, index-1]:
+  #      assert contact_old[i].lastname == contact_orm[i].lastname
+  #      assert contact_old[i].firstname == contact_orm[i].firstname
 
 
 
